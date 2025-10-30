c524351081042529aea83ce2068da9dd75ddfdee
Júlia Fonseca <juliafonsecca6@gmail.com>
2025-10-30 15:16:14 -0300

    Delete contador.py

--- contador.py
+++
@@ -1,9 +0,0 @@
-def contar_vogais(texto):
-    vogais = "aeiouAEIOU"
-    return sum(1 for letra in texto if letra in vogais)
-
-# Teste:
-if __name__ == "__main__":
-    frase = "welcome to my dev world"
-    total = contar_vogais(frase)
-    print(f"Número de vogais na frase: {total}")
